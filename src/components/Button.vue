f8b2e22d331cb70e174606aac236412b57d6ef5e
krlmari <krylova_m_v@inbox.ru>
2024-12-06 00:43:19 +0100

    initial commit

---
+++ src/components/Button.vue
@@ -0,0 +1,45 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup>
+
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+});
+
+const emit = defineEmits(['click']);
+
+const onClick = () => {
+  emit('click');
+};
+</script>
+
+<template>
+  <button 
+    type="button" 
+    @click="onClick" 
+>
+    {{ label }}
+  </button>
+</template>
+
+<!-- 
+<style scoped>
+  button {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
+    border-radius: 1000px;
+    border: none;
+    background: #cacaca; 
+    color: #313131;
+    font-size: 16px;
+    font-weight: bold;
+    cursor: pointer;
+  }
+</style>
+-->
+
+
